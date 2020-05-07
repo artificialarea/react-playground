2a387f34d0724bf56ffba208762db4d5b1c2076b
Sacha Sedriks <sachased@gmail.com>
2020-05-07 11:50:57 -0700

    per instructions on https://courses.thinkful.com/react-v1/checkpoint/6 made 'Split' component (.js) and App.css.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Split from './composition/Split';
+import './App.css';
 
 function App() {
   return (
     <main className='App'>
       {/* content goes here */}
+      <Split />
+      <br />
+      <Split />
+      <Split />
+      <Split />
     </main>
   );
 }
---
+++ src/composition/Split.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Split() {
+  return (
+    <div className="split">
+      This is the content of the split component. Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quasi molestias aspernatur natus quo aut cumque sit? Recusandae suscipit omnis eos quisquam natus nihil porro voluptates voluptas, error sint possimus!
+    </div>
+  );
+}
+
+export default Split;
+
