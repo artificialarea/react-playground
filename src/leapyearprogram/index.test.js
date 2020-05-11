fa63a6dcc7d3cbd67e1eb3d3a0cecabab01482f9
Sacha Sedriks <sachased@gmail.com>
2020-05-11 15:15:09 -0700

    added Jest index.test.js for my first unit test.

---
+++ src/leapyearprogram/index.test.js
@@ -0,0 +1,42 @@
+import leapYear from './index.js';
+
+// the 'it' function is invoke by Jest
+
+it('should NOT be a leap year if divisible by 100, not 400', () => {
+  expect(leapYear(1900)).toBe(false);
+});
+
+it('should be a leap year if divisible by 4, not 100', () => {
+  expect(leapYear(1984)).toBe(true);
+});
+
+it('should NOT be a leap year if not divisible by 4', () => {
+  expect(leapYear(1983)).toBe(false);
+});
+
+it('should be a leap year if divisible by 400', () => {
+  expect(leapYear(2000)).toBe(true);
+});
+
+it('should throw an error for years before 1582', () => {
+  expect(() => {
+    leapYear(1568);
+  }).toThrow();
+});
+
+// ^^^^^^^
+// these are shorthand tests that skip const declarations, like so...
+
+// it('should NOT be a leap year if divisible by 100, not 400', () => {
+//   const input = 1900;
+//   const expectedOutput = false; 
+//   const actualOutput = leapYear(input);
+//   expect(actualOutput).toBe(expectedOutput);
+// });
+
+// it('should be a leap year if divisible by 4, not 100.', () => {
+//   const input = 1984;
+//   const expectedOutput = true; 
+//   const actualOutput = leapYear(input);
+//   expect(actualOutput).toBe(expectedOutput);
+// });
