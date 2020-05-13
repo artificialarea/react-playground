5b39daab4c125c82c84617c6fcade0336262f1a9
Sacha Sedriks <sachased@gmail.com>
2020-05-13 15:54:53 -0700

    updated <TheDate /> 'life-cycle' with setInterval to update state.

--- src/state/TheDate.js
+++ src/state/TheDate.js
@@ -11,10 +11,23 @@ class TheDate extends React.Component {
       list: [1, 2, 3],
       obj: { nested: 'object', yes: true },
     }
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      // this.state.datetime = new Date() // doesn't work, as React needs to be notified about the update. If we directly mutate the component state the React won't know about the change. 
+      // Enter the method, setState: the setState method takes an object argument, that the component will then merge with the exisiting state, without requiring to change the render (because the new object has the same key as the existing state).// Everytime setState is called, the React component will re-trigger render.
+      this.setState({ datetime: new Date() });
+    }, 1000); 
+    console.log('componentDidMount')
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval)
   }
 
   render() {
-    console.log(this.state);
+    console.log('render')
     return <div>{this.state.datetime.toLocaleString()}</div>
   }
 }
