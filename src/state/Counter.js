515649dd1e3d366a2b8f761006a8cb58ce9ae5a1
Sacha Sedriks <sachased@gmail.com>
2020-05-13 21:54:53 -0700

    updated so new count rendered each button click.

--- src/state/Counter.js
+++ src/state/Counter.js
@@ -6,9 +6,13 @@ class Counter extends React.Component {
     count: 0,
   }
 
-  // The takeaway rule of thumb:
+  // General Rule:
   // When adding an event listener function to a React class, use an arrow function to make sure that this is referring to the class instance.
   handleButtonClick = () =>  {
+    const newCount = this.state.count + 1;
+    this.setState({
+      count: newCount
+    })
     console.log(this.state.count);
   }
 
