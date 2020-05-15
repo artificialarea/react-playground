6ee8a935505ea21d30f1f47ba7204d53b9ead045
Sacha Sedriks <sachased@gmail.com>
2020-05-15 13:35:56 -0700

    refactoring <Test /> render logic into separate functions.

--- src/state/Tabs.js
+++ src/state/Tabs.js
@@ -7,23 +7,32 @@ class Tabs extends React.Component {
 
   };
 
-  render() {
-    const buttons = this.props.tabs.map((tab, index) => (
+  // refactoring render logic into separate functions
+
+  renderButtons() {
+    return this.props.tabs.map((tab, index) => (
       <button key={index}>
         {tab.name}
       </button>
     ))
+  }
+
+  renderContent() {
     const currentTab = this.props.tabs[0]
     return (
+      <div className='content'>
+        {currentTab.content}
+      </div>
+    )
+  }
+
+  render() {
+    return (
       <div>
-        {buttons}
+        {this.renderButtons()}
         {/* Conditional Rendering: so smoke test doesn't fail, 
         only show content when the tabs array isn't empty */}
-        {this.props.tabs.length && (
-          <div className='content'>
-            {currentTab.content}
-          </div>
-        )}
+        {this.props.tabs.length && this.renderContent()}
       </div>
     )
   }
