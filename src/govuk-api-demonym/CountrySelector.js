e7080e875606107149b18861496c8434b5785a72
Sacha Sedriks <sachased@gmail.com>
2020-05-30 12:53:25 -0700

    finished demonym exercise.

--- src/govuk-api-demonym/CountrySelector.js
+++ src/govuk-api-demonym/CountrySelector.js
@@ -2,7 +2,19 @@ import React from 'react'
 import './CountrySelector.css'
 
 export default class CountrySelector extends React.Component {
+
+  changeSelection(value) {
+    if (value === "None") {
+      this.props.changeHander(null);
+    } else {
+      const country = this.props.countries.find(country => country.name === value);
+      this.props.changeHandler(country);
+      console.log(country);
+    }
+  }
+
   render() {
+
     const options = this.props.countries
       .map((country, i) => 
         <option value={country.name} key={i}>{country.name}</option>
@@ -12,7 +24,10 @@ export default class CountrySelector extends React.Component {
       <div className="country_selector">
         <form>
           <label htmlFor="country">Select a Country:</label>
-          <select id="country" name="country">
+          <select 
+            id="country" 
+            name="country" 
+            onChange={(event) => this.changeSelection(event.target.value)}>
             <option value="None">Select one...</option>
             {options}
           </select>
