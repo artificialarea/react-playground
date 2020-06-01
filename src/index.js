eca4b5d4fed9c97b3c24fa4ed3c3f08523bb3336
Sacha Sedriks <sachased@gmail.com>
2020-06-01 15:14:18 -0700

    updated view for this branch.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom'; // req. npm install
 import './index.css';
 
 // import App from './App'; // enable to see non-power-toe exercises
-// import App from './paltrows-power-toes/App'; // re:RouteRouter
-import App from './govuk-api-demonym/App'; // re:API+Promises
+import App from './paltrows-power-toes/App'; // re:RouteRouter
+// import App from './govuk-api-demonym/App'; // re:API+Promises
 
 ReactDOM.render(
   <BrowserRouter>
