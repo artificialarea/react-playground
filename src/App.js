d9e7d869efdef81604a671b56feaab3d5e5217cc
Sacha Sedriks <sachased@gmail.com>
2020-09-13 12:07:25 -0700

    Update README

--- src/App.js
+++ src/App.js
@@ -13,29 +13,29 @@ import Accordion from './state-drills/Accordion';
 import STORE from './state-drills/accordion-store';
 
 //////////////////////////////////////////////////////////
-//
+
 // [v1] Component Composition, 
 // per: https://courses.thinkful.com/react-v1/checkpoint/6
-//
+
 // [v2] Default Props and Class Components,
 // per: https://courses.thinkful.com/react-v1/checkpoint/7
-//
+
 // [v3] Intro to Testing,
 // per: https://courses.thinkful.com/react-v1/checkpoint/8
-//
+
 // [v4] React State,
 // per: https://courses.thinkful.com/react-v1/checkpoint/9
-//
+
 // [v5] Event Handlers and Conditional Rendering,
 // per: https://courses.thinkful.com/react-v1/checkpoint/10
-//
+
 // [v6] React Router
 // per: https://courses.thinkful.com/react-v1/checkpoint/14#setting-up-react-router
 // src: https://github.com/tomatau/paltrows-power-toes
-//
+
 // [v7] API Requests
 // per: https://courses.thinkful.com/react-v1/checkpoint/13
-//
+
 //////////////////////////////////////////////////////////
 
 // FOR <SPLITZ /> ////////////////////////////////////////
